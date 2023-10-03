06157cfe304763f2e532312e1237f14d254dd8e7
AChicken703 <146892347+AChicken703@users.noreply.github.com>
2023-10-03 15:03:17 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,79 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . f 2 f e e e e f f . . . . 
+. . f 2 2 2 f e e e e f f . . . 
+. . f e e e e f f e e e f . . . 
+. f e 2 2 2 2 e e f f f f . . . 
+. f 2 e f f f f 2 2 2 e f . . . 
+. f f f e e e f f f f f f f . . 
+. f e e 4 4 f b e 4 4 e f f . . 
+. . f e d d f 1 4 d 4 e e f . . 
+. . . f d d d e e e e e f . . . 
+. . . f e 4 e d d 4 f . . . . . 
+. . . f 2 2 e d d e f . . . . . 
+. . f f 5 5 f e e f f f . . . . 
+. . f f f f f f f f f f . . . . 
+. . . f f f . . . f f . . . . . 
+`;
+            case "image2":
+            case "myImage0":return img`
+................................
+................................
+........................bb......
+.......................bbb......
+.....................bbbb..ffff.
+...................bbbbbffffff..
+..................bbfffffffff...
+................ffffffffffff....
+...f..........ff444fffffffffff..
+...ff.......ff4442222ffffeeeeeff
+...fff....ff444222f2222222222eee
+....fffffff444222ff222222222222e
+....f44444442222ff2222ff22222222
+....f22222222222222222fff2222222
+....f2f2222222222222222fff222222
+....f222222222222222222222222222
+.....f22222222222222222222222222
+.....f22222222222222222222222222
+......f2211111111222222222222222
+......1111.1.1..2222222222222222
+.......1.......22222222222eee222
+.............11222222222eeeeeee2
+..........1..2222222eeeeeeeeeeee
+..........11122222eeeffffeeeeeee
+...........f22222eeff....fffeeee
+............f22efff.........ffee
+.............fff..............fe
+...............................f
+................................
+................................
+................................
+................................
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
